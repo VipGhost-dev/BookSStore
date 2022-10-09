e92a29e342b226bbb0a4b8cefec799d0e836f284
Vip_Ghost <lied00@mail.ru>
2022-10-10 00:48:42 +0300

    3 from 5

--- app/src/main/java/com/example/booksstore/MainActivity.java
+++ app/src/main/java/com/example/booksstore/MainActivity.java
@@ -99,19 +99,7 @@ public class MainActivity extends AppCompatActivity {
                             EditText publisherEdit = findViewById(R.id.PublisherTxt);
                             EditText ageEdit = findViewById(R.id.AgeTxt);
                             EditText genreEdit = findViewById(R.id.GenreTxt);
-                            try {
-                                Books.removeAllViews();
-                                String query = "Select * from Books where ID = '"+changebtn.getId()+"'";
-                                ResultSet resultSet1 = statement.executeQuery(query);
-                                nameEdit.setText(resultSet1.getString(2));
-                                authorEdit.setText(resultSet1.getString(3));
-                                ageEdit.setText(resultSet1.getString(4));
-                                publisherEdit.setText(resultSet1.getString(5));
-                                genreEdit.setText(resultSet1.getString(6));
-
-                            } catch (SQLException e) {
-                                e.printStackTrace();
-                            }
+                            Button changeBTN = findViewById(R.id.Change);
                             nameEdit.setVisibility(View.VISIBLE);
                             authorEdit.setVisibility(View.VISIBLE);
                             publisherEdit.setVisibility(View.VISIBLE);
@@ -121,6 +109,43 @@ public class MainActivity extends AppCompatActivity {
                             Button addbtn = findViewById(R.id.add);
                             refreshbtn.setVisibility(View.INVISIBLE);
                             addbtn.setVisibility(View.INVISIBLE);
+                            changeBTN.setVisibility(View.VISIBLE);
+                            try {
+                                Books.removeAllViews();
+                                String query = "Select * from Books where ID = '"+changebtn.getId()+"'";
+                                ResultSet resultSet1 = statement.executeQuery(query);
+                                while (resultSet1.next()) {
+                                    changeBTN.setId(Integer.parseInt(resultSet1.getString(1)));
+                                    nameEdit.setText(resultSet1.getString(2));
+                                    authorEdit.setText(resultSet1.getString(3));
+                                    ageEdit.setText(resultSet1.getString(4));
+                                    publisherEdit.setText(resultSet1.getString(5));
+                                    genreEdit.setText(resultSet1.getString(6));
+                                }
+
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                            changeBTN.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                      try {
+                                          String query = "UPDATE Books SET Name = '" + nameEdit.getText() + "', Author = '" + authorEdit.getText() + "', Year = '" + ageEdit.getText() + "', Publisher = '" + publisherEdit.getText() + "', Genre = '" + genreEdit + "' where ID = '" + changeBTN.getId() + "'";
+                                          statement.executeUpdate(query);
+
+                                    } catch (SQLException e) {
+                                        e.printStackTrace();
+                                    }
+                                    nameEdit.setVisibility(View.INVISIBLE);
+                                    authorEdit.setVisibility(View.INVISIBLE);
+                                    publisherEdit.setVisibility(View.INVISIBLE);
+                                    ageEdit.setVisibility(View.INVISIBLE);
+                                    genreEdit.setVisibility(View.INVISIBLE);
+                                    changeBTN.setVisibility(View.INVISIBLE);
+                                    refreshbtn.setVisibility(View.VISIBLE);
+                                    addbtn.setVisibility(View.VISIBLE);
+                                }
+                            });
                         }
                     });
                     dbOutputRow.addView(changebtn);
@@ -136,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onClick(View v) {
                             try {
-                                String query = "DELETE from Books where ID = '"+deletebtn.getId()+"'";
+                                String query = "DELETE from Books where ID = '" + deletebtn.getId() + "'";
                                 statement.executeUpdate(query);
                             } catch (SQLException e) {
                                 e.printStackTrace();
@@ -179,5 +204,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, Bookcreate.class));
     }
 
+    public void changeBook(View v)
+    {
+
+    }
 
 }
